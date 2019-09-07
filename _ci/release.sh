8909172a54c1f67b6a393ee636bf4379163d3ffd
Alan Davis <alan.davis@alfresco.com>
2019-09-07 08:41:31 +0100

    Undo changes included in the last merge that were only intended to do a development release.

--- _ci/release.sh
+++ _ci/release.sh
@@ -7,7 +7,6 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 # For PR builds only execute a Dry Run of the release
 [ "${TRAVIS_PULL_REQUEST}" = "false" ] && DRY_RUN="" || DRY_RUN="-DdryRun"
-DRY_RUN=""
 
 # Travis CI runner work on DETACHED HEAD, so we need to checkout the release branch
 git checkout -B "${TRAVIS_BRANCH}"
@@ -19,8 +18,6 @@ mvn -B \
     ${DRY_RUN} \
     "-Darguments=-DskipTests -Dmaven.javadoc.skip -Dadditionalparam=-Xdoclint:none" \
     release:clean release:prepare release:perform \
-    -DreleaseVersion=repo4639.1 \
-    -DdevelopmentVersion=1.0.2.7-SNAPSHOT \
     -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
     -Dusername=alfresco-build \
     -Dpassword=${GIT_PASSWORD}
