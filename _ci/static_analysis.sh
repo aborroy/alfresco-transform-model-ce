808a7968fe9a955a408575cd2657e8a8bd7507a7
Denis Ungureanu <Denis.Ungureanu1@ness.com>
2020-03-19 18:26:04 +0200

    ATS-468 : Add Veracode (SAST & SCA) scans to Transform Service Travis builds (#52)
       - add SCA & SAST
       - add sandbox name
       - run static scans based on git branch
       - use maven plugin for sourceclear scans
       - filter logs

---
+++ _ci/static_analysis.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "=========================== Starting Static Analysis Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+# Run in a sandbox for every branch, run normally on master
+[ "${TRAVIS_BRANCH}" != "master" ] && RUN_IN_SANDBOX="-sandboxname Transform_Model" || RUN_IN_SANDBOX=""
+
+java -jar vosp-api-wrappers-java-$VERACODE_WRAPPER_VERSION.jar -vid $VERACODE_API_ID \
+     -vkey $VERACODE_API_KEY -action uploadandscan -appname "Transform Service" \
+     ${RUN_IN_SANDBOX} -createprofile false \
+     -filepath target/alfresco-transform-model-*.jar \
+     -version "$TRAVIS_JOB_ID - $TRAVIS_JOB_NUMBER" -scantimeout 3600
+
+popd
+set +vex
+echo "=========================== Finishing Static Analysis Script =========================="
